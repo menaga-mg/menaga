6d516abd59c5ff118b7d0e2994d5b11d2c6d9732
menaga-mg <47380183+menaga-mg@users.noreply.github.com>
2019-02-06 16:10:19 +0530

    Create positivenegative.java

---
+++ positivenegative.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class positivenegative
+{
+    public static void main(String[] args) 
+    {
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        if(number > 0)
+        {
+            System.out.println("Positive");
+        }
+        else if(number < 0)
+        {
+            System.out.println(" Negative ");
+        }
+        else
+        {
+            System.out.println(" Zero");
+        }
+    }
